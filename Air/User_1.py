e62765afcb2d1077235e5d45171cce718737518b
Wexily <147994442+Wexily@users.noreply.github.com>
2023-11-11 12:58:01 +0300

    User_1.0

--- Air/User_1.py
+++ Air/User_1.py
@@ -13,7 +13,6 @@ import sys
 class UserMain(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
