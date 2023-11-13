33fd19435a4790c807d98416ceb53a4f72bffa95
Stanislawsi-benty <olimpik.up@mail.ru>
2023-11-14 00:37:55 +0300

    sign and admin is complete

--- Air/main_1.py
+++ Air/main_1.py
@@ -1,12 +1,14 @@
 import sys
-from PyQt6.QtWidgets import QLineEdit
+from PyQt6.QtWidgets import QLineEdit, QMessageBox, QInputDialog
 from Air_db.sign_db import *
+from Air_db.ticket_db import *
 from py_interfaces.sign_ui import Ui_Enter
 from py_interfaces.admin_main_ui import Ui_AdminWindowMain
 
 
 class EnterWindow(QtWidgets.QMainWindow, Ui_Enter):
     '''Инициализация класса входа'''
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_Enter()
@@ -22,6 +24,7 @@ class EnterWindow(QtWidgets.QMainWindow, Ui_Enter):
         self.sign_db.mysignal.connect(self.signal_handler)
 
     '''функция проверки есть ли символы в полях логина и пароля'''
+
     def check_input(funct):
         def wrapper(self):
             for line_edit in self.base_line_edit:
@@ -32,17 +35,22 @@ class EnterWindow(QtWidgets.QMainWindow, Ui_Enter):
         return wrapper
 
     '''Возвращает сигнал(окно) после авторизации и регистрации'''
+
     def signal_handler(self, value):
         QtWidgets.QMessageBox.about(self, 'Оповещение', value)
 
     '''функция авторизации'''
+
     @check_input
     def auth(self):
         login = self.ui.lineEdit.text()
         password = self.ui.lineEdit_2.text()
         answer = self.sign_db.thr_login(login, password)
+
         if answer == 'Админ':
             signwindow.close()
+            self.ui.lineEdit.clear()
+            self.ui.lineEdit_2.clear()
             adminWindow.show()
         elif answer == 'Кассир':
             print("Кассир")
@@ -50,6 +58,7 @@ class EnterWindow(QtWidgets.QMainWindow, Ui_Enter):
             print("Клиент")
 
     '''функция регистрации'''
+
     @check_input
     def reg(self):
         login = self.ui.lineEdit.text()
@@ -62,17 +71,75 @@ class EnterWindow(QtWidgets.QMainWindow, Ui_Enter):
 
 class AdminWindow(QtWidgets.QMainWindow):
     '''Инициализация класса вдмина'''
+
     def __init__(self):
         super().__init__()
         self.ui = Ui_AdminWindowMain()
         self.ui.setupUi(self)
 
-        self.ui.pushButton_add.clicked.connect(self.to_sign)
+        self.ui.pushButton_exit.clicked.connect(self.to_sign)
+        self.ui.pushButton_add.clicked.connect(self.addTickets)
+        self.ui.pushButton_del.clicked.connect(self.delTicket)
+        self.ui.pushButton_date.clicked.connect(self.change_date)
+        self.ui.pushButton_cost.clicked.connect(self.change_price)
+
+        self.ticket_db = TicketSet()
+
+        self.value = []
+        self.loadTickets()
 
     '''Функция выхода в окно входа'''
+
     def to_sign(self):
         adminWindow.close()
         signwindow.show()
+
+    def addTickets(self):
+        self.ticket_db.ticket_add()
+        self.ui.listWidget.clear()
+        self.loadTickets()
+
+    def loadTickets(self):
+        self.value = self.ticket_db.ticket_show()
+        self.ui.listWidget.addItems(
+            [str(*[f"Номер рейса: {i[1]} | Откуда: {i[2]} | Куда: {i[3]} | Время вылета: {i[4]}"
+                   f" | Дата вылета: {i[5]} | Цена: {i[6]} ₽ | Количество {i[7]}"]) for i in self.value])
+
+    def delTicket(self):
+        currentIndex = self.ui.listWidget.currentRow()
+        item = self.ui.listWidget.item(currentIndex)
+        if item is None:
+            return
+
+        question = QMessageBox.question(self, "Удаление билета", "Хотите удалить билет: " + '\n' + item.text() + '?')
+
+        if question == QMessageBox.StandardButton.Yes:
+            item = self.ui.listWidget.takeItem(currentIndex)
+            self.ticket_db.ticket_del(item.text().split())
+
+    def change_date(self):
+        currentIndex = self.ui.listWidget.currentRow()
+        item = self.ui.listWidget.item(currentIndex)
+
+        if item is not None:
+            text, ok = QInputDialog.getText(self, "Изменить дату рейса", "Введите дату:")
+            if text and ok is not None:
+                self.ticket_db.data_change(item.text().split('|'), text)
+
+        self.ui.listWidget.clear()
+        self.loadTickets()
+
+    def change_price(self):
+        currentIndex = self.ui.listWidget.currentRow()
+        item = self.ui.listWidget.item(currentIndex)
+
+        if item is not None:
+            price, ok = QInputDialog.getInt(self, "Изменить цену рейса", "Введите цену:")
+            if price and ok is not None:
+                self.ticket_db.cost_change(item.text().split('|'), price)
+
+        self.ui.listWidget.clear()
+        self.loadTickets()
 
 
 if __name__ == '__main__':
